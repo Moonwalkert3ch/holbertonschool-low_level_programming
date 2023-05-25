058128dfa9740dc0b3f71a5b68d812b37b6e06a0
Moonwalkert3ch <6620@holbertonstudents.com>
2023-05-24 20:20:48 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,9 +3,9 @@
  * main - Program that prints a line using printf function
  *
  * Return: Always 0 (Sucess)
- */
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n);
-return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
