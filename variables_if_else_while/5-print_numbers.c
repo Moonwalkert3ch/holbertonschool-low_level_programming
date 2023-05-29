184a93ee3fab48c375e7bbd1210a2b1d16026128
Moonwalkert3ch <6620@holbertonstudents.com>
2023-05-29 13:43:26 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/**
+ * File: 5-print_numbers.c
+ * author: Rachael Bradford
+*/
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/** Main -Prints all single digit numbers of bae 10.
+ * RETURN - 0 ALWAYS SUCCESS
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
