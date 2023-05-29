ef0b73b49821d3a361ae16582d15df99e2f84af9
Moonwalkert3ch <6620@holbertonstudents.com>
2023-05-29 16:58:27 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* author: Rachael Bradford File: 8-print_base16.c */
+
+/**
+ * main - prints all the numbers of base 16 in lower case.
+ *
+ * Return - Always 0 success
+*/
+int main(void)
+{
+int n;
+char ch;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
