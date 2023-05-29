f6e6713d63b703e604e4c47a20fb04e000a072c5
Moonwalkert3ch <6620@holbertonstudents.com>
2023-05-29 12:47:26 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Main- Prints alphabet in lowercase
+ * RETURN 0 succes
+ *
+*/
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar (i);
+	putchar ('\n');
+	return (0);
+}
