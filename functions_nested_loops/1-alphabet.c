78a6ff36575a9e666ee470a1a48da9a126b791b1
Moonwalkert3ch <6620@holbertonstudents.com>
2023-05-30 21:01:20 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** main - Entry point
+ *  definition: print the alphabet in lowercase followed by a new line.
+ *  Return: Always 0 success
+*/
+
+void print_alphabet(void)
+{
+char c;
+
+for(c = 'a'; c<= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
