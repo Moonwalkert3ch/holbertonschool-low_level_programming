bb94fe8c2504e546f5deb1aaf2cbb502d26313c9
Moonwalkert3ch <6620@holbertonstudents.com>
2023-05-30 20:31:32 -0700

    0-putchar

--- functions_nested_loops/_putchar.c
+++ functions_nested_loops/_putchar.c
@@ -1,9 +1,9 @@
-#include <main.h>
+#include <unistd.h>
 /** 
  * _putchar - writes the chracter c to stdout
  * Return: always 0 success
 */
-int _putchar(char c)
+int _putchar (char c)
 {
 return (write(1, &c, 1));
 }
