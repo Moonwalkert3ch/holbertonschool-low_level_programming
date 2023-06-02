5702a193512cf5bc471ee8fc7c02514188bf2354
Moonwalkert3ch <6620@holbertonstudents.com>
2023-06-01 17:21:31 -0700

    main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef main.h
+#define main.h
+
 int _putchar(char c);
 
 void print_alphabet(void);
@@ -16,10 +19,12 @@ int print_last_digit(int);
 
 void jack_bauer(void);
 
-void times_table();
+void times_table(void);
 
 int add(int, int);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
+
+#endif
