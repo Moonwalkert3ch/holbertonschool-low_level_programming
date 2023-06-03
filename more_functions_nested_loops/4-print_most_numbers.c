59cbf063d408caa80cd80f82700b53af1d17b856
Moonwalkert3ch <6620@holbertonstudents.com>
2023-06-03 16:12:54 -0700

    5-more_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_most_numbers - Print the numbers from 0 to 9, followed by a new line.
+ * Desscription: Print all except 2 and 4.
+ * Return: result
 */
-int main(void)
+
+void print_most_numbers(void)
 {
-	print_most_numbers();
-	return (0);
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	if (number != '2' && number != '4')
+	_putchar(number);
+	_putchar('\n');
 }
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers from 0 to 14,
+ * description: followed by a new line.
+ * Return: void
+*/
+
+void more_numbers(void)
+{
+	int number;
+       	char c;
+
+	for (number = 0; number < 10; number++)
+	{
+	for (c = 0; c <= 14; c++)
+	{
+	if (c > 9)
+	{
+	_putchar((c / 10) + '0');
+	}
+	_putchar((c % 10) + '0');
+	}
+	_putchar('\n');
+	}
+}
