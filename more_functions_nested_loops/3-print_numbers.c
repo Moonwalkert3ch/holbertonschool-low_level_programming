6571d39dcc7137dabc506fa9c86eb572aa553a5b
Moonwalkert3ch <6620@holbertonstudents.com>
2023-06-03 15:05:00 -0700

    3-print_numbers

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9.
- * @numbers: integer to be determined.
+ * print_numbers - prints the numbers from 0 to 9, followed by a new line.
+ * @number: integer to be determined.
  * Return: mothing.
 */
 void print_numbers(void)
