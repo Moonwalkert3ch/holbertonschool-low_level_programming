a02cb828a1c5a64db73425905b7f190867f6a396
Moonwalkert3ch <6620@holbertonstudents.com>
2023-06-03 17:03:07 -0700

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,27 +7,20 @@
 */
 int main(void)
 {
-	int n;
+	int n = 1;
 
-	for (n = 1; n <= 100; n++)
+	for (; n < 100; n++)
 	{
 	if ((n % 3 == 0) && (n % 5 == 0))
-	{
-	printf("fizzBuzz");
-	}
+		printf("FizzBuzz ");
 	else if (n % 3 == 0)
-	{
-	printf("fizz");
-	}
+		printf("Fizz ");
 	else if (n % 5 == 0)
-	{
-	printf("%d", n);
+		printf("Buzz ");
+	else
+		printf("%d", n);
 	}
-	if (n != 100)
-	{
-	printf(" ");
-	}
-	}
-	printf("\n");
+	printf("Buzz\n");
+
 	return (0);
 }
