fa92510e690ada744ec6cd28b09759d05b202adc
Moonwalkert3ch <6620@holbertonstudents.com>
2023-06-12 18:29:31 -0700

    _putchar.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -8,15 +8,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-	int aLen = sizeof(*a) / sizeof(*a[0]);
-
-	for (i = 0; i < aLen; i++)
+	int i;
+	int j;
+	
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < aLen; j++)
-		{
+		for (j = 0; j < 8; i++)
 			_putchar(a[i][j]);
-		}
 		_putchar('\n');
 	}
 }
