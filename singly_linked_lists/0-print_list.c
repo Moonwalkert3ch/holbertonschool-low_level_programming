d14dd0883dce3c8468a8554fdd58ca8689bf8c2f
Moonwalkert3ch <6620@holbertonstudents.com>
2023-07-16 20:50:09 -0700

    singly linked lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print list elements.
+ * @h: points to the head.
+ * Description: prints all elements in the list.
+ * Return: number of nodes, if
+ * NULL that will be printed
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t elements;
+
+	elements = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		printf("[0] (nil)");
+
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	h = h->next;
+	elements++;
+
+	return (elements);
+}
