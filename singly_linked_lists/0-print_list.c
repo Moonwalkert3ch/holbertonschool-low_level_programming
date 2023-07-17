a9a3234a0e77a45e4150b1fe52c870b81176f459
Moonwalkert3ch <6620@holbertonstudents.com>
2023-07-16 20:58:29 -0700

    adjustments made to file singly linked list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,10 +16,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		printf("[0] (nil)");
+		printf("[%d] %s\n", 0, "(nil)");
 
 	}
 	printf("[%d] %s\n", h->len, h->str);
+
 	h = h->next;
 	elements++;
 
