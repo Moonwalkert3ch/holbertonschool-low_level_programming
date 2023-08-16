cc14b56d165e43cc8c1c69c93ba7a370f5b2b3f7
Moonwalkert3ch <6620@holbertonstudents.com>
2023-08-16 14:54:43 -0700

    fix bettty probs

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,9 +4,12 @@
 
 /**
  * linear_search - searches for desired value in an
- *  array of integers using linear search
+ * array of integers using linear search
+ * @array: Pointer to the first element of the array
+ * to search in
  * @size: number of elements in the array
  * @value: value to search for
+ * Return: first index where value is located
 */
 
 int linear_search(int *array, size_t size, int value)
@@ -15,9 +18,9 @@ size_t n;
 
 if (array == NULL)
 {
-return (-1);
+	return (-1);
 }
-for ( n = 0; n < size; n++)
+for (n = 0; n < size; n++)
 {
 printf("Value checked array[%ld] = [%d]\n", n, array[n]);
 
