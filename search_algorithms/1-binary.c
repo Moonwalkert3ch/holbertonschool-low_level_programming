75126a56eb034f8b96960c81f2f7811f31870d25
Moonwalkert3ch <6620@holbertonstudents.com>
2023-08-16 18:11:05 -0700

    running through checker

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -15,7 +15,7 @@
 int binary_search(int *array, size_t size, int value)
 {
 	int n;
-	int middle;
+	size_t middle;
 	int left = 0;
 	int right = size - 1;
 
@@ -35,7 +35,7 @@ printf("Searching in array: %d\n", array[n]);
 if (array[middle] == value)
 {
 if ((middle == 0 || array[middle - 1] != value)
-	       && (middle == size -1 || array[middle + 1] != value))
+	       && (middle == size - 1 || array[middle + 1] != value))
 return (middle);
 }
 else if (middle == 0 || array[middle - 1] != value)
@@ -46,7 +46,7 @@ else
 {
 right = middle - 1;
 }
-else if (array[middle] < value)
+if (array[middle] < value)
 {
 left = middle + 1;
 }
